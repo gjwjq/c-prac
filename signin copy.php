c616f036b5996236226046e5ac174b17d5000121
gjwjq <jaykwon615@gmail.com>
2026-01-09 20:52:16 +0900

    완벽한 로그인

--- signin copy.php
+++ signin copy.php
@@ -13,3 +13,15 @@ if(!$user) {
   back('id가 존재하지 않아요');
   exit;
 }
+
+$salt = $user['salt'];
+$enc_pw = hash('sha256', $pw . $salt);
+
+if($enc_pw === $user['pw']) {
+  $_SESSION['user'] = $user;
+  alert('로그인 성공');
+  move('index.php');
+} else {
+  back('비밀번호가 틀림');
+  exit;
+}
--- signin.php
+++ signin.php
@@ -1,28 +1,27 @@
 <?php
-  session_start();
-  require_once "db.php";
-  require_once "lib.php";
+require_once 'db.php';
+require_once 'lib.php';
+session_start();
 
-  $id = $_POST['id'];
-  $pw = $_POST['pw'];
+$id = $_POST['id'];
+$pw = $_POST['pw'];
 
-  $sql = "SELECT * FROM user WHERE id = '$id'";
-  $user = DB::fetch($sql);
+$sql = "SELECT * FROM user WHERE id = '$id'";
+$user = DB::fetch($sql);
 
-  if(!$user) {
-    alert('id가없음');
-    exit;
-  } 
+if(!$user) {
+  back('id가 존재하지 않아요');
+  exit;
+}
 
-  $salt = $user['salt'];
-  $enc_pw = hash('sha256', $salt . $pw);
+$salt = $user['salt'];
+$enc_pw = hash('sha256', $pw . $salt);
 
-  if($enc_pw === $user['pw']) {
-    $_SESSION['user'] = $user;
-    alert('로그인 성공');
-    move('index.php');
-  } else {
-    alert('비밀번호가 틀림');
-    move('index.php');
-    exit;
-  }
+if($enc_pw === $user['pw']) {
+  $_SESSION['user'] = $user;
+  alert('로그인 성공');
+  move('index.php');
+} else {
+  back('비밀번호가 틀림');
+  exit;
+}
